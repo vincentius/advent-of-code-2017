922150817a4f9c6595a76b515a37f04517e65d60
Vincent Baggerman <vincent.baggerman@abstractive.be>
2017-12-05 08:47:56 +0100

    Day 5

---
+++ 5.py
@@ -0,0 +1,36 @@
+inp = """"""
+
+inp = inp.splitlines()
+inp = [int(x) for x in inp]
+position = 0
+step = 0
+while True:
+    try:
+        item = inp[position]
+    except Exception:
+        break
+    step += 1
+    inp[position] += 1
+    position += item
+
+print(step)
+
+inp = """"""
+
+inp = inp.splitlines()
+inp = [int(x) for x in inp]
+position = 0
+step = 0
+while True:
+    try:
+        item = inp[position]
+    except Exception:
+        break
+    step += 1
+    if item > 2:
+        inp[position] -= 1
+    else:
+        inp[position] += 1
+    position += item
+
+print(step)
